b11b2adfb40d86e7fd33b28d56e83817629df826
Vlad Osmianski <vlaosm@gmail.com>
2025-04-09 14:33:15 +0300

    Add a package

---
+++ package/src/PackageServiceProvider.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Osmianski\Package;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+
+}
---
+++ package/src/functions.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types=1);
+
+function say_world(): string
+{
+    return __('messages.world');
+}
